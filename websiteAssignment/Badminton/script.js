31a89ded0ab4cc1908e97397b6249a5b2a356e1f
u7568823 <u7568823@anu.edu.au>
2024-04-15 23:54:57 +1000

    put 4 photo into photo gallery

--- websiteAssignment/Badminton/script.js
+++ websiteAssignment/Badminton/script.js
@@ -7,8 +7,11 @@ document.addEventListener('DOMContentLoaded', function () {
       e.preventDefault();
 
       const contentId = this.getAttribute('data-content');
+
       window.location.hash = contentId;
       updateContent(contentId);
+
+
     });
   });
   // Load content based on current hash
@@ -41,6 +44,10 @@ function updateContent(contentId) {
     case 'join':
       content.innerHTML = '<h2>Join Our Club</h2><p>Become a member of our badminton community and enjoy the game.</p>';
       break;
+    // case 'assignment':
+
+    //   break;
+
     default:
 
       getCustomPage(contentId)
@@ -59,5 +66,37 @@ function getCustomPage(htmlName) {
       console.error('Error loading the My Passion content:', error);
     });
 }
+
+
+// slideShow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   var slides = document.querySelectorAll('.mySlides img');
+//   slides.forEach(function (img) {
+//     img.onerror = function () {
+//       this.src = 'LinDan.png'; // Fallback image
+//     };
+//     img.src = img.getAttribute('data-src'); // Only set src after onerror is assigned
+//   });
+// });
 
 
